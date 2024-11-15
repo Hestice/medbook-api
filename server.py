0a36fc7a66c3653be012b0ace5ca12d2e96d8842
Hestice <custillano@gmail.com>
2024-11-15 12:54:43 +0800

    misc: initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Api test"
+
+@app.route("/api/home", methods=['GET'])
+def return_home():
+    return jsonify({
+        'message': "Hello world!"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
